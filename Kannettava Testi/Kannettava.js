0d10365e91592b7108a14c49b373825d33970d6b
Ilmari-Lindberg <ilmari.lindberg@gmail.com>
2021-03-05 20:13:01 +0200

    Kannettavan testi / Ilmari Lindberg / 5.3.2021

---
+++ Kannettava Testi/Kannettava.js
@@ -0,0 +1,11 @@
+function testi(num) {
+  if (num < 20) {
+    console.log("Luku on alle 20");
+  }
+  else if (num == 20) {
+    console.log("Luku on tasan 20");
+  }
+  else {
+    console.log("Luku on yli 20");
+  }
+}
